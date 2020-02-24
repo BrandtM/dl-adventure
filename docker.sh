bea964371cf980ce8cfb14c9c5a394b15f5c9e34
BrandtM <marius.brandt@hotmail.com>
2020-02-24 21:24:45 +0100

    Improve docker.sh

--- docker.sh
+++ docker.sh
@@ -6,11 +6,6 @@ docker image build -t python-dcgan:latest .
 cd trainer
 docker image build -t python-dcgan-trainer:latest .
 
-cd ../generator
-docker image build -t python-dcgan-generator:latest .
-
 cd ../../../..
 
-# For now uncomment these lines to enable training/generating
-# docker run --gpus all --rm -it -v `pwd`/python/dcgan:/application python-dcgan-trainer:latest
-#docker run --rm -it -v `pwd`/python/dcgan:/application python-dcgan-generator:latest
+docker run --gpus all --rm -it -v `pwd`/python/dcgan:/application python-dcgan-trainer:latest
